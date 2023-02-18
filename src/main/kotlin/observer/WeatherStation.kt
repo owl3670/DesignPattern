7531ecd951f68278b937edff3b5a0481fabbe882
owl3670 <gnltjr123@gmail.com>
2023-02-18 15:45:58 +0900

    + heat index display 추가

--- src/main/kotlin/observer/WeatherStation.kt
+++ src/main/kotlin/observer/WeatherStation.kt
@@ -3,6 +3,7 @@ package observer
 import observer.data.WeatherData
 import observer.display.CurrentConditionalDisplay
 import observer.display.ForecastDisplay
+import observer.display.HeatIndexDisplay
 import observer.display.StatisticsDisplay
 
 class WeatherStation {
@@ -13,6 +14,7 @@ class WeatherStation {
             val currentDisplay = CurrentConditionalDisplay(weatherData)
             val statisticsDisplay = StatisticsDisplay(weatherData)
             val forecastDisplay = ForecastDisplay(weatherData)
+            val heatIndexDisplay = HeatIndexDisplay(weatherData)
             weatherData.setMeasurements(80f, 65f, 30.4f)
             weatherData.setMeasurements(82f, 70f, 29.2f)
             weatherData.setMeasurements(78f, 90f, 29.2f)
